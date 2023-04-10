301b480fefd7ec791c2abec79c0de0a285b75d04
Eliel Nfinic <elielmathe@nfinic.com>
2023-04-10 23:28:30 +0200

    Fixing bot

--- stack_overflow.js
+++ stack_overflow.js
@@ -3,19 +3,22 @@ exports.get_stack_questions = async (last_10_minutes) => {
         const axios = require('axios');
         //Get last 10 minutes time boundaries
         const now = new Date();
-        const now_time = now.getTime();
+        const now_time = Math.floor(now.getTime() / 1000);
+        console.log("Now time", now_time);
         const from = new Date(now.getTime() - 10 * 60000);
-        const from_time = from.getTime();
+        console.log("From time", from);
+        const from_time = Math.floor(from.getTime() / 1000);
 
-        //sort=votes&min=10&fromdate=1293840000&todate=1294444800
+        const query_url = `https://api.stackexchange.com/2.3/questions?fromdate=${from_time}&todate=${now_time}&sort=creation&tagged=starknet&site=stackoverflow`;;
 
         var config = {
             method: 'get',
             maxBodyLength: Infinity,
-            url: `https://api.stackexchange.com/2.3/questions?site=stackoverflow&tagged=starknet`, //&fromdate=${from_time}&todate=${now_time}`,
+            url: query_url // `https://api.stackexchange.com/2.3/questions?fromdate=1681084800&order=desc&tagged=starknet&site=stackoverflow`,
         };
 
         const res = await axios(config);
+        console.log(res.data);
         return res.data;
     } catch (err) {
         console.log(err);

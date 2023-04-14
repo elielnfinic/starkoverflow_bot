c50b1ed73f016ada75448ad14e92d8734c8876e0
Eliel Nfinic <elielmathe@nfinic.com>
2023-04-14 22:02:12 +0200

    Update

--- app.js
+++ app.js
@@ -1,41 +1,7 @@
-const TelegramBot = require('node-telegram-bot-api');
-const dotenv = require('dotenv');
-const { get_stack_questions } = require('./stack_overflow');
+const { run } = require(".");
 
-dotenv.config();
+const axios = require('axios');
+const { listen_telegram_messages } = require('./telegram');
 
-const token = process.env.TELEGRAM_API_TOKEN;
-
-const bot = new TelegramBot(token, { polling: true });
-
-// listen to incoming messages
-bot.on('message', async (msg) => {
-    console.log(msg);
-    const chatId = msg.chat.id;
-    console.log(chatId);
-    const res = await get_stack_questions();
-    let questions = '';
-    for(let i = 0; i < res.items.length && questions.length < 3500; i++) {
-        questions += `${i + 1}. ${res.items[i].title}\nLink: ${res.items[i].link}\n\n`;
-    }
-    bot.sendMessage(chatId, questions);
-});
-
-const send_message = async (chat_id, message) => {
-    const res = await get_stack_questions();
-    let questions = '';
-    for(let i = 0; i < res.items.length && questions.length < 3500; i++) {
-        questions += `${i + 1}. ${res.items[i].title}\nLink: ${res.items[i].link}\n\n`;
-    }
-    // console.log(res);
-    if(questions)
-        bot.sendMessage(-994700852, questions);
-}
-
-send_message();
-
-
-//bot.sendMessage(-994700852, "We learn from mistakes https://www.pubcm.com");
-
-
-
+// run();
+listen_telegram_messages();
